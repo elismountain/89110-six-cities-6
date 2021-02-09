bba197fa8640326cf674731ab1a50ce5fb982fc9
elismountain <echospb@mail.ru>
2021-02-09 02:56:03 +0000

    favorites and login components created

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Main from '../main/main';
+import Login from '../login/login';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 const App = ({adCount, cities}) => {
   return (
-    <Main adCount={adCount} cities={cities} />
+    <BrowserRouter>
+    <Switch>
+      <Route path="/">
+        <Main adCount={adCount} cities={cities} />
+      </Route>
+      <Route path="/login" exact>
+        <Login />
+        </Route>
+      <Route path="/favorites" exact>
+        <Favorites />
+      </Route>
+
+      </Switch>
+    </BrowserRouter>
   )
 }
 
