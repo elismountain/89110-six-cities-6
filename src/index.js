2dc1f47b019fe3d83c357dc708dc94cb7c2f1623
elismountain <echospb@mail.ru>
2021-03-01 19:12:36 +0000

    index page store added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+import {createStore} from 'redux';
+import {reducer} from './store/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from 'react-redux';
+
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
+
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+);
 
 ReactDOM.render(
     <App
