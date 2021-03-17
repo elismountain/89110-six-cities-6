731640485dbe8aaaef3d654a2af795f955a433ea
elismountain <echospb@mail.ru>
2021-03-17 01:13:00 +0000

    figthinf with offers

---
+++ src/components/main/main-offers.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import {offerPropType} from '../../prop-types';
+import OffersSorting from '../offers-sorting/offers-sorting';
+import PlacesList from '../places-list/places-list';
+import Map from '../map/map';
+import {Cities, CardTypes} from '../../const';
+
+
+const MainOffers = (props) => {
+  const {activeCity, offers} = props;
+  const [activeCard, setActiveCard] = useState(null);
+
+  const handleCardMouseEnter = (selectedCard) => {
+    setActiveCard(selectedCard);
+  };
+
+  const handleCardMouseLeave = () => {
+    setActiveCard(null);
+  };
+
+  return (
+    <div className="cities__places-container container">
+      <section className="cities__places places">
+        <h2 className="visually-hidden">Places</h2>
+        <b className="places__found">{offers.length} places to stay in {activeCity}</b>
+        <OffersSorting/>
+        <PlacesList
+          offers={offers}
+          cardType={CardTypes.MAIN}
+          onCardMouseEnter={handleCardMouseEnter}
+          onCardMouseLeave={handleCardMouseLeave}
+        />
+      </section>
+      <div className="cities__right-section">
+        <section className="cities__map map">
+          <Map city={activeCity} points={offers} activePoint={activeCard}/>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+MainOffers.propTypes = {
+  activeCity: PropTypes.oneOf(Object.values(Cities)),
+  offers: PropTypes.arrayOf(offerPropType)
+};
+
+export default MainOffers;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,29 +2,17 @@ import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../header/header';
-import PlacesList from '../places-list/places-list';
-import OffersSorting from '../offers-sorting/offers-sorting';
 import CitiesList from '../cities-list/cities-list';
 import {offerPropType} from '../../prop-types';
-import Map from '../map/map';
-import {Cities, CardTypes} from '../../const';
+import MainOffers from '../main/main-offers';
+
+import {Cities} from '../../const';
 import {sortOffers} from '../../sorting';
 import MainEmpty from '../main/main-empty';
 import cn from 'classnames';
 
 const Main = (props) => {
   const {cityOffers, activeCity} = props;
-  const cityLocation = cityOffers.length ? cityOffers[0].city.location : {};
-
-  const [activeCard, setActiveCard] = useState(null);
-
-  const handleCardMouseEnter = (selectedCard) => {
-    setActiveCard(selectedCard);
-  };
-
-  const handleCardMouseLeave = () => {
-    setActiveCard(null);
-  };
 
   return (
     <div className="page page--gray page--main">
@@ -37,24 +25,8 @@ const Main = (props) => {
           </section>
         </div>
         <div className="cities">
-          {cityOffers.length
-            ? <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{cityOffers.length} places to stay in {activeCity}</b>
-                <OffersSorting/>
-                <PlacesList
-                  offers={cityOffers}
-                  cardType={CardTypes.MAIN}
-                  onCardMouseEnter={handleCardMouseEnter}
-                  onCardMouseLeave={handleCardMouseLeave}/>
-              </section>
-              <div className="cities__right-section">
-                <section className="cities__map map">
-                  <Map city={cityLocation} points={cityOffers} activeMarker={activeCard}/>
-                </ section>
-              </div>
-            </div>
+          {cityOffers.length ?
+            <MainOffers offers={cityOffers} activeCity={activeCity} />
             :
             <MainEmpty activeCity={activeCity} />
           }
