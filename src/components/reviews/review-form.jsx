2b911b579761990e56eaad86ac57458a8d8e82e5
elismountain <echospb@mail.ru>
2021-04-06 00:03:26 +0100

    fix: fixed review textholder

--- src/components/reviews/review-form.jsx
+++ src/components/reviews/review-form.jsx
@@ -1,15 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
+
 import {connect} from 'react-redux';
 import {sendReview} from '../../store/api-actions';
-import {Ratings, COMMENT_MIN_LENGTH} from '../../const';
 
+import {Ratings, COMMENT_MIN_LENGTH} from '../../const';
 
 const ReviewForm = (props) => {
   const {id, onSubmit} = props;
   const initialState = {'rating': null, 'comment': ``};
   const [reviewForm, setReviewForm] = useState(initialState);
   const [disabled, setDisabled] = useState(true);
+
+  const handleFieldChange = (evt) => {
+    const {name, value} = evt.target;
+    setReviewForm({...reviewForm, [name]: value});
+  };
 
   useEffect(() => {
     setDisabled(!reviewForm.rating || reviewForm.comment.length < COMMENT_MIN_LENGTH);
@@ -21,18 +27,14 @@ const ReviewForm = (props) => {
     setReviewForm({...reviewForm, ...initialState});
   };
 
-  const handleFieldChange = (evt) => {
-    const {name, value} = evt.target;
-    setReviewForm({...reviewForm, [name]: value});
-  };
-
   return (
     <form className="reviews__form form" action="#" method="post" onSubmit={handleSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
         {Ratings.map(({value, title}) => (
           <React.Fragment key={value}>
-            <input className="form__rating-input visually-hidden"
+            <input
+              className="form__rating-input visually-hidden"
               name="rating"
               value={value}
               id={`${value}-stars`}
@@ -40,9 +42,8 @@ const ReviewForm = (props) => {
               onChange={handleFieldChange}
               checked={value === +reviewForm.rating}
             />
-
             <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title={title}>
-              <svg className="form__star-image" width={37} height={37}>
+              <svg className="form__star-image" width="37" height="33">
                 <use xlinkHref="#icon-star"></use>
               </svg>
             </label>
@@ -50,22 +51,20 @@ const ReviewForm = (props) => {
         ))}
       </div>
 
-      <textarea className="reviews__textarea form__textarea" id="review" name="review"
+      <textarea className="reviews__textarea form__textarea" id="comment" name="comment"
         placeholder="Tell how was your stay, what you like and what can be improved"
         value={reviewForm.comment}
-        onChange={handleFieldChange}
-      />
-
+        onChange={handleFieldChange} />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
-          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay
+          with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
         <button className="reviews__submit form__submit button" type="submit" disabled={disabled}>Submit</button>
       </div>
     </form>
   );
 };
-
 
 ReviewForm.propTypes = {
   id: PropTypes.number.isRequired,
